d1c915bfcf1a42e95404b32c8b194017d3bd71cc
Suman Saurabh <sudofr@Sumans-MBP.lan>
2022-09-22 09:19:28 -0500

    first

---
+++ README.txt
@@ -0,0 +1,16 @@
+What should this program do:
+
+1. Program generates a passowrd                                             - DONE
+    - I want it to save it in a sqlite databse which I can access. 
+    - How to save it the best way like a locker ?
+    - How can make it available all the time to me ?
+2.  May be later through a GUI but for now:
+    This program should report to me in the morning 
+    the weather, top news through some news channel and then ask me
+    if I want to set up the agenda for the day.
+3. 
+
+
+============== In this section, right now I will describe how the program is developing with time ==============
+Right now, I am trying to write a program which when started should provide me the weather. I will 
+call this program right now "Sati"
---
+++ main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+#include <random>
+#include <ctime>
+#include <cstdlib>
+#include <chrono>
+#include <thread>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+string string_randomizer(string &str, int len_pass){
+    srand((unsigned) time(0));
+    string loose_string;            // the collected string which will be returned.
+    for (int i= 0; i< len_pass; i++){
+        loose_string = loose_string + str[rand()%len_pass];
+    }
+std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+    return loose_string;
+}
+
+void print_stuff(){
+    cout << "*************"<<endl;
+}
+
+
+int main(){
+    int len_pass;
+    string passwd;
+    // Initialize string array
+    string pass_token[4]= { "0123456789", "abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "$&_#!" };
+    srand((unsigned) time(0));
+    cout << "Enter the length of the desired passoword: "<< endl;
+    try {
+    // Block of code to try
+        cin >> len_pass;
+        if (len_pass>=6){
+            cout << "Minimum passowrd length requirement satisfied! Working magic now!"<< endl;
+                for (int j = 0; j< len_pass; j++){
+                // sampling various candidate characters for a passoword component
+        for (int i = 0; i <4 ; i++){
+            if (i == 0){
+                int num = rand()%10;
+                passwd += pass_token[i][num];
+            }
+            else if (i ==1){
+                int num = rand()%26;
+                passwd += pass_token[i][num];
+            }
+            else if (i ==1){
+                int num = rand()%26;
+                passwd += pass_token[i][num];
+            }
+            else {
+                int num = rand()%5;
+                passwd += pass_token[i][num];
+            }
+        }
+       
+    }
+    std::thread second (print_stuff);
+    passwd = string_randomizer(passwd, len_pass);
+    
+    // synchronize threads:
+    //first.join();                // pauses until first finishes
+    second.join();               // pauses until second finishes
+
+    cout << passwd << endl;
+        }
+        else{
+            throw (len_pass); // Throw an exception when a problem arise
+        }
+    }
+    catch (int len_pass) {
+    // Block of code to handle errors
+    cout << "The length of passowrd asked for was: " << len_pass << " .This won't provide a great security. Request aborted!" << endl;
+    }
+       
+}
+
